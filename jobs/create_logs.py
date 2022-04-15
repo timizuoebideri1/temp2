4e673e1641f286b9e8b9e752187a0b863fbfbd32
Timizuo <timizuoebideri@gmail.com>
2022-04-15 10:57:17 +0100

    erper

--- jobs/create_logs.py
+++ jobs/create_logs.py
@@ -12,7 +12,7 @@ class CustomFieldCreation(Job):
         description = "Provision a new branch site"
 
     def run(self, data, commit):
-        while True:
+        for x in range(500):
             time.sleep(1)
             site = Site.objects.first()
             self.log_success(obj=site, message="Created new site")
@@ -23,6 +23,7 @@ class CustomFieldCreation(Job):
             return '\n'.join(output)
 
     def post_run(self):
-        for x in range(500):
-            self.log_info(message="POST RUN REACHED INFO")
+        for x in range(2000):
+            time.sleep(1)
+            self.log_info(message=f"POST RUN REACHED INFO - {x}")
 
