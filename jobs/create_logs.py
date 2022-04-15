d417576eacc1036c319fca5e1e957084861a7891
Timizuo <timizuoebideri@gmail.com>
2022-04-15 11:05:13 +0100

    aanother

--- jobs/create_logs.py
+++ jobs/create_logs.py
@@ -14,16 +14,17 @@ class CustomFieldCreation(Job):
     def run(self, data, commit):
         for x in range(500):
             time.sleep(1)
-            site = Site.objects.first()
-            self.log_success(obj=site, message="Created new site")
-            self.log_info(obj=site, message="Creating Custom Fields")
+            
+            site = Site.objects.create(name=f"Site {x}", slug=f"site-{x}")
+            self.log_success(obj=site, message=f"Created new {site}")
+            self.log_info(obj=site, message=f"Created new {site}")
 
             output = ["device creation", "device okay"]
 
             return '\n'.join(output)
 
-    def post_run(self):
-        for x in range(2000):
-            time.sleep(1)
-            self.log_info(message=f"POST RUN REACHED INFO - {x}")
+    # def post_run(self):
+    #     for x in range(2000):
+    #         time.sleep(1)
+    #         self.log_info(message=f"POST RUN REACHED INFO - {x}")
 
